90b74b5f993b00c9190068dee81d0598d5a536c7
yunawinaya <gedeyuna@gmail.com>
2023-08-16 00:47:26 +0800

    bottom navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import {
   AppBar,
   Avatar,
   Box,
+  BottomNavigation,
+  BottomNavigationAction,
   Container,
   IconButton,
   Menu,
@@ -18,6 +20,9 @@ import { AuthContext } from "../context/AuthContext";
 import { auth } from "../lib/firebase";
 import { MaterialUISwitch } from "./Switch";
 import NewHotelModal from "./NewHotelModal";
+import HomeIcon from "@mui/icons-material/Home";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import FeedIcon from "@mui/icons-material/Feed";
 
 export const Navbar = () => {
   const { currentUser, setDarkMode, isAdmin } = useContext(AuthContext);
@@ -25,6 +30,7 @@ export const Navbar = () => {
 
   const [anchorEl, setAnchorEl] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [value, setValue] = useState(0);
 
   const open = Boolean(anchorEl);
 
@@ -49,111 +55,133 @@ export const Navbar = () => {
   };
 
   return (
-    <AppBar position="sticky" color="inherit">
-      <Container maxWidth="lg">
-        <Toolbar
-          disableGutters
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingY: 1.2,
-          }}
-        >
-          <Typography
-            onClick={() => navigate("/hotels")}
-            sx={{ cursor: "pointer" }}
-            variant="h6"
-            color="inherit"
-            component="div"
-            fontWeight={"bold"}
+    <>
+      <AppBar position="sticky" color="inherit">
+        <Container maxWidth="lg">
+          <Toolbar
+            disableGutters
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingY: 1.2,
+            }}
           >
-            NomadsLyfe
-          </Typography>
-          {isAdmin && (
-            <Button color="error" onClick={handleOpenModal}>
-              Add Accommodation
-            </Button>
-          )}
-          <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-            <FormGroup sx={{ display: { xs: "none", md: "flex" } }}>
-              <FormControlLabel
-                control={
-                  <MaterialUISwitch
-                    onChange={() => setDarkMode((prev) => !prev)}
-                  />
-                }
-              />
-            </FormGroup>
-
-            <Typography
-              onClick={() => setDarkMode((prev) => !prev)}
-              sx={{ display: { xs: "block", md: "none" }, cursor: "pointer" }}
-              fontSize={15}
-              variant="h6"
-              color="inherit"
-              component="a"
-            >
-              DarkMode
-            </Typography>
             <Typography
               onClick={() => navigate("/hotels")}
               sx={{ cursor: "pointer" }}
-              fontSize={15}
               variant="h6"
               color="inherit"
-              component="a"
+              component="div"
+              fontWeight={"bold"}
             >
-              Home
+              NomadsLyfe
             </Typography>
+            {isAdmin && (
+              <Button color="error" onClick={handleOpenModal}>
+                Add Accommodation
+              </Button>
+            )}
+            <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+              <FormGroup sx={{ display: { xs: "none", md: "flex" } }}>
+                <FormControlLabel
+                  control={
+                    <MaterialUISwitch
+                      onChange={() => setDarkMode((prev) => !prev)}
+                    />
+                  }
+                />
+              </FormGroup>
 
-            <IconButton
+              <Typography
+                onClick={() => setDarkMode((prev) => !prev)}
+                sx={{ display: { xs: "block", md: "none" }, cursor: "pointer" }}
+                fontSize={15}
+                variant="h6"
+                color="inherit"
+                component="a"
+              >
+                DarkMode
+              </Typography>
+
+              <IconButton
+                id="demo-positioned-menu"
+                onClick={handleClick}
+                size="small"
+                aria-controls={open ? "demo-positioned-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <Avatar
+                  src={currentUser?.photoURL}
+                  sx={{ width: 32, height: 32 }}
+                />
+              </IconButton>
+            </Box>
+          </Toolbar>
+          {isModalOpen && (
+            <NewHotelModal show={isModalOpen} handleClose={handleCloseModal} />
+          )}
+
+          <div>
+            <Menu
               id="demo-positioned-menu"
-              onClick={handleClick}
-              size="small"
-              aria-controls={open ? "demo-positioned-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-            >
-              <Avatar
-                src={currentUser?.photoURL}
-                sx={{ width: 32, height: 32 }}
-              />
-            </IconButton>
-          </Box>
-        </Toolbar>
-        {isModalOpen && (
-          <NewHotelModal show={isModalOpen} handleClose={handleCloseModal} />
-        )}
-
-        <div>
-          <Menu
-            id="demo-positioned-menu"
-            aria-labelledby="demo-positioned-button"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-          >
-            <MenuItem
-              onClick={() => {
-                navigate("/my-profile");
-                handleClose();
+              aria-labelledby="demo-positioned-button"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
               }}
             >
-              My Profile
-            </MenuItem>
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-          </Menu>
-        </div>
-      </Container>
-    </AppBar>
+              <MenuItem
+                onClick={() => {
+                  navigate("/my-profile");
+                  handleClose();
+                }}
+              >
+                My Profile
+              </MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </div>
+        </Container>
+      </AppBar>
+      <AppBar position="fixed" color="inherit" sx={{ top: "auto", bottom: 0 }}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            label="Home"
+            onClick={() => navigate("/hotels")}
+            icon={<HomeIcon />}
+          />
+          <BottomNavigationAction
+            label="Whatsapp"
+            onClick={() =>
+              window.open(
+                "https://api.whatsapp.com/send?phone=6281337953460",
+                "_blank"
+              )
+            }
+            icon={<WhatsAppIcon />}
+          />
+          <BottomNavigationAction
+            label="News"
+            onClick={() => navigate("/news")}
+            icon={<FeedIcon />}
+          />
+        </BottomNavigation>
+      </AppBar>
+    </>
   );
 };
