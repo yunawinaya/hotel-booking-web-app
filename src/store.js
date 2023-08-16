2686fe77385b2eb93ced3c7b9a0adf83ce1aa8e1
yunawinaya <gedeyuna@gmail.com>
2023-08-16 22:21:29 +0800

    minor

--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import hotelsReducer from "./features/posts/hotelSlice";
 import newsReducer from "./features/posts/newsSlice";
-import thunk from "redux-thunk";
 
 export default configureStore({
   reducer: {
     hotels: hotelsReducer,
     news: newsReducer,
   },
-  middleware: [thunk],
 });
