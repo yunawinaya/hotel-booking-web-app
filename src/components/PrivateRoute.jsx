dd4d3ba6a8168f6cc4388e34394559cdecd435a4
yunawinaya <gedeyuna@gmail.com>
2023-08-17 22:07:41 +0800

    solving bug

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,18 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../lib/firebase";
 
 const PrivateRoute = ({ component: Component }) => {
+  const [authChecked, setAuthChecked] = useState(false);
   const navigate = useNavigate();
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
+      setAuthChecked(true);
       if (!user) {
         navigate("/");
       }
     });
   }, [navigate]);
 
-  return <Component />;
+  return authChecked ? <Component /> : null;
 };
 
 export default PrivateRoute;
